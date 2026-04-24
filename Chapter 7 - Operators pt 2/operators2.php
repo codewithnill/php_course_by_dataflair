d949b2af09beaaab4be701cddf8bebc8508f854a
codewithnill <nbnill.hmlnmh@gmail.com>
2026-04-25 00:21:34 +0600

    operators pt 2.

---
+++ Chapter 7 - Operators pt 2/operators2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operators 2</title>
+</head>
+<body>
+    <center>
+        <?php
+           // logical operators
+            $a=50;
+            $b=30;
+            if($a>40 && $b>20){
+                echo "both conditions are true";
+            }
+            else{
+                echo "one of the conditions is false";
+            }
+
+
+        ?>
+    </center>
+    
+</body>
+</html>
